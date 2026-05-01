2779fcfd9db6b38b70aea0604624c24a3ee42382
hyemin <hyemin6647@naver.com>
2026-05-01 18:49:17 +0900

    ♻️ be: 코드 가독성을 위한 줄바꿈 정리

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,10 +8,12 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "ok"}
+
 @app.get("/test-db")
 def test_db():
     with engine.connect() as conn:
         result = conn.execute(text("SELECT 1"))
         row = result.fetchone()
         return {"result": row[0]}
+      
 app.include_router(tutors.router)
